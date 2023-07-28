f52ebd5376e5178a143fc282130e793193216b7a
이학준 <lhjohn30@gmail.com>
2023-07-28 10:58:34 +0900

    feat : test 부분 주석처리

--- src/test/java/com/example/centossamplespringboot/CentosSampleSpringbootApplicationTests.java
+++ src/test/java/com/example/centossamplespringboot/CentosSampleSpringbootApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.centossamplespringboot;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CentosSampleSpringbootApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.centossamplespringboot;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CentosSampleSpringbootApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
